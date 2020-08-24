c21b69f441cb79861a2ea60e073417c51cc45e1c
unknown <dschleicherwbs@gmail.com>
2020-08-24 20:04:00 +0200

    first

---
+++ precache-manifest.ef1db1a49c70a81e589d6ecc09311013.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff3b3822f75e8363d73b",
+    "url": "css/app.199b3191.css"
+  },
+  {
+    "revision": "eb620899e70ed41dcb79b65ff5f37ae3",
+    "url": "index.html"
+  },
+  {
+    "revision": "ff3b3822f75e8363d73b",
+    "url": "js/app.47fd22d4.js"
+  },
+  {
+    "revision": "be9b631cfca1325468f1",
+    "url": "js/chunk-vendors.1d5d92eb.js"
+  },
+  {
+    "revision": "002ae7f90fc33282e627e95c01b9c9a5",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
